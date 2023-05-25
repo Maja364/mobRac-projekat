509db4a4ed1b4b725523ba7e65deefa8860f6524
Maja Aleksic <majaaleksic62@gmail.com>
2023-05-25 17:39:19 +0200

    Deklarisana komponenta koja omogucava koriscenje TaskElement komponente

---
+++ src/app/tasks/explore/explore.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ExplorePageRoutingModule } from './explore-routing.module';
+
+import { ExplorePage } from './explore.page';
+import { TaskElementComponent } from '../task-element/task-element.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ExplorePageRoutingModule
+  ],
+  declarations: [ExplorePage, TaskElementComponent]
+})
+export class ExplorePageModule {}
