3b558b11872eebeb24110b43009d35ef44ad53a2
Maja Aleksic <majaaleksic62@gmail.com>
2023-05-25 18:12:10 +0200

    Postavljen canLoad na rute tasks i subjects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,32 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'tasks',
+    loadChildren: () => import('./tasks/tasks.module').then(m => m.TasksPageModule),
+    canLoad: [AuthGuard]
+  },
+
+  {
+    path: 'subjects',
+    loadChildren: () => import('./subjects/subjects.module').then( m => m.SubjectsPageModule),
+    canLoad: [AuthGuard]
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'log-in',
+    loadChildren: () => import('./auth/log-in/log-in.module').then( m => m.LogInPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./auth/register/register.module').then( m => m.RegisterPageModule)
   },
 ];
 
