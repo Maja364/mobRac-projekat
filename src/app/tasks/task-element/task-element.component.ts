9d247bdb6bed5163d3a3504a6009144186205369
Maja Aleksic <majaaleksic62@gmail.com>
2023-05-25 17:36:57 +0200

    Kreiramo Task-element i prosledjujemo podatke iz parent-a tj. Explore stranice

---
+++ src/app/tasks/task-element/task-element.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Task } from '../task.model';
+
+@Component({
+  selector: 'app-task-element',
+  templateUrl: './task-element.component.html',
+  styleUrls: ['./task-element.component.scss'],
+})
+export class TaskElementComponent  implements OnInit {
+
+  @Input() task: Task = {id:'t3', title: 'Seminarski iz Projektovanja softvera', description: 'Seminarski zavrsiti do kraja maja', imgUrl:'https://i.pinimg.com/736x/ac/c2/9e/acc29eb7588b23d2cf548e32eff5ce9c.jpg'};
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
