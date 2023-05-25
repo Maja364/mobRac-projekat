9ca159c89ed34bcc9e5270eb89c0c50d50fdcbfe
Maja Aleksic <majaaleksic62@gmail.com>
2023-05-25 16:56:30 +0200

    Kreirana Tasks stranica

---
+++ src/app/tasks/tasks.page.ts
@@ -0,0 +1,11 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.page.html',
+  styleUrls: ['./tasks.page.scss'],
+})
+export class TasksPage {
+
+
+}
