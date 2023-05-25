e3b2a7c2efc2e4f7bab22ae3d15e6f2a04b9bdbd
Maja Aleksic <majaaleksic62@gmail.com>
2023-05-25 17:00:20 +0200

    Kreirane metode zivotnog ciklusa stranice

---
+++ src/app/subjects/subjects.page.ts
@@ -0,0 +1,38 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subjects',
+  templateUrl: './subjects.page.html',
+  styleUrls: ['./subjects.page.scss'],
+})
+export class SubjectsPage implements OnInit, OnDestroy {
+
+  constructor() {
+    console.log('constructor');
+  }
+
+  ngOnInit() {
+    console.log('ngOnIt');
+  }
+
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter');
+  }
+
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter');
+  }
+
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave');
+  }
+
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
+
+}
