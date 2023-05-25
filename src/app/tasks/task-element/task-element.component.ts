eed44bc709d444f569d56ba3729db9fc11394633
Maja Aleksic <majaaleksic62@gmail.com>
2023-05-25 17:59:17 +0200

    Kreiran izgled elementa niza u Tasku

--- src/app/tasks/task-element/task-element.component.ts
+++ src/app/tasks/task-element/task-element.component.ts
@@ -8,6 +8,7 @@ import { Task } from '../task.model';
 })
 export class TaskElementComponent  implements OnInit {
 
+  //addImport dekorator
   @Input() task: Task = {id:'t3', title: 'Seminarski iz Projektovanja softvera', description: 'Seminarski zavrsiti do kraja maja', imgUrl:'https://i.pinimg.com/736x/ac/c2/9e/acc29eb7588b23d2cf548e32eff5ce9c.jpg'};
 
   constructor() { }
