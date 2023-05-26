bb8fa0fe36bfd5ca8a73e21b6a1c1b5c3e6a200e
Jovana Aleksic <jaleksic23@gmail.com>
2023-05-27 01:05:52 +0200

    modal za task

--- src/app/tasks/task-modal/task-modal.component.ts
+++ src/app/tasks/task-modal/task-modal.component.ts
@@ -1,13 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, NgModule, OnInit, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { IonicModule, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-task-modal',
   templateUrl: './task-modal.component.html',
   styleUrls: ['./task-modal.component.scss'],
+  standalone:true,
+  imports: [IonicModule,CommonModule,FormsModule,RouterModule]
 })
+
 export class TaskModalComponent  implements OnInit {
 
-  constructor() { }
+  @ViewChild('f',{static:true}) form!: NgForm;
+  @Input() title!:string;
+
+  constructor(private modalCtrl:ModalController) { }
+
+  onCancel(){
+    this.modalCtrl.dismiss();
+  }
+  onAddTask(){
+    if(!this.form.valid){
+      return;
+    }
+    this.modalCtrl.dismiss({taskData:{title:this.form.value['title'],description:this.form.value['text']}},'confirm');
+  }
 
   ngOnInit() {}
 
