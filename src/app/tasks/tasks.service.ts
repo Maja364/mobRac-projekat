81b8a1e24fe2d29f008c2b316312a0d88c063180
Jovana Aleksic <jaleksic23@gmail.com>
2023-05-28 00:17:45 +0200

    metode za komuniciranje sa Firebase-om

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Task } from './task.model';
+import { HttpClient } from '@angular/common/http';
+
+interface TaskData{
+  title:string;
+  description:string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +17,18 @@ export class TasksService {
     {id: 't2', title: 'Kolokvijum iz Projektovanja softvera', description: 'Kolokvijum spremiti do petka', imgUrl: 'https://i.pinimg.com/564x/bc/89/7c/bc897c914c753731b1ac2060f784b836.jpg'}
   ];
 
-  //constructor() { }
+  constructor(private http:HttpClient) { }
+
+  addTask(title:string, description:string){
+    return this.http.post<{name:string}>('https://task-app-96268-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',{
+      title,
+      description
+    });
+  }
+
+  getTasks(){
+    return this.http.get<{[key:string]:TaskData}>('https://task-app-96268-default-rtdb.europe-west1.firebasedatabase.app/tasks.json')
+  }
 
   getTask(id: string){
     return this.tasks.find((t) => t.id === id);
