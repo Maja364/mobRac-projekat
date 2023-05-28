c76a37a28b44d57379aa4331fef4121935e18495
Jovana Aleksic <jaleksic23@gmail.com>
2023-05-29 00:00:08 +0200

    cuvanje zadataka sa userId-jem

--- src/app/tasks/task.model.ts
+++ src/app/tasks/task.model.ts
@@ -1,6 +1,11 @@
-export interface Task {
-    id: string;
-    title: string;
-    description: string;
-    imgUrl: string;
+export class Task {
+
+    constructor(public id:string, public title:string, public description:string, public imgUrl:string, public userId:string){
+
+    }
+
+    // id: string;
+    // title: string;
+    // description: string;
+    // imgUrl: string;
 }
--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -2,10 +2,13 @@ import { Injectable } from '@angular/core';
 import { Task } from './task.model';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, map, switchMap, take, tap } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 interface TaskData{
   title:string;
   description:string;
+  imgUrl:string;
+  userId:string;
 }
 
 @Injectable({
@@ -16,11 +19,10 @@ export class TasksService {
   private _tasks=new BehaviorSubject<Task[]>([]);
 
   oldTasks: Task[] = [
-    {id: 't1', title: 'Projekat iz Mobilnog racunarstva', description: 'Projekat zavrsiti do 1. juna', imgUrl: 'https://i.pinimg.com/736x/ac/c2/9e/acc29eb7588b23d2cf548e32eff5ce9c.jpg'},
-    {id: 't2', title: 'Kolokvijum iz Projektovanja softvera', description: 'Kolokvijum spremiti do petka', imgUrl: 'https://i.pinimg.com/564x/bc/89/7c/bc897c914c753731b1ac2060f784b836.jpg'}
+    {id: 't1', title: 'Projekat iz Mobilnog racunarstva', description: 'Projekat zavrsiti do 1. juna', imgUrl: 'https://i.pinimg.com/736x/ac/c2/9e/acc29eb7588b23d2cf548e32eff5ce9c.jpg',userId:'xx'},
   ];
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient, private authService:AuthService) { }
 
   get tasks(){
     return this._tasks.asObservable();
@@ -28,24 +30,36 @@ export class TasksService {
 
   addTask(title:string, description:string){
     let generatedId: string;
-    return this.http.post<{name:string}>('https://task-app-96268-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',{
-      title,
-      description
-    }).pipe(switchMap((resData)=>{
-      generatedId=resData.name;
-      return this.tasks;
-      
-    }),
-    take(1),
-    tap((tasks)=>{
-      this._tasks.next(tasks.concat({
-        id:generatedId,
-        title,
-        description,
-        imgUrl:'https://i.pinimg.com/564x/bc/89/7c/bc897c914c753731b1ac2060f784b836.jpg'
-      }));
+    let newTask:Task;
 
-    }));
+    return this.authService.userId.pipe(
+      take(1),
+      switchMap(userId=>{
+        newTask=new Task(
+          null!, 
+          title, 
+          description, 
+          'https://i.pinimg.com/564x/bc/89/7c/bc897c914c753731b1ac2060f784b836.jpg',
+          userId!
+          );
+          return this.http.post<{name:string}>('https://task-app-96268-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',newTask );
+
+      }),
+      
+      take(1),
+      switchMap((resData)=>{
+        generatedId=resData.name;
+        return this.tasks;
+        
+      }),
+      take(1),
+      tap((tasks)=>{
+        newTask.id=generatedId;
+        this._tasks.next(tasks.concat(newTask));
+      }
+        
+      ));
+
   }
 
   getTasks(){
@@ -55,12 +69,7 @@ export class TasksService {
 
       for(const key in tasksData){
         if(tasksData.hasOwnProperty(key)){
-          tasks.push({
-            id:key,
-            title: tasksData[key].title,
-            description: tasksData[key].description,
-            imgUrl:'https://png.pngtree.com/png-clipart/20190117/ourmid/pngtree-hand-painted-teachers-day-a-stack-of-books-book-png-image_432486.jpg',
-          });
+          tasks.push(new Task(key,tasksData[key].title,tasksData[key].description,tasksData[key].imgUrl,tasksData[key].userId));
         }
       }
 
