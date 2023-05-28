bacfc90bb6a3c6b3a68292a6412cc44624869561
Jovana Aleksic <jaleksic23@gmail.com>
2023-05-28 22:28:42 +0200

    BehaviorSubject (null greska)

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { User } from './user.model';
 
 interface AuthResponseData{
   kind:string;
@@ -24,6 +26,7 @@ interface UserData{
 export class AuthService {
 
   private _isUserAuthenticated = false;
+  private _user=new BehaviorSubject<User>(null);
 
   constructor(private http:HttpClient) {
 
@@ -35,20 +38,36 @@ export class AuthService {
   register(user:UserData){
     this._isUserAuthenticated = true;
     return this.http.post<AuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseAPIKey}`,
-    {email:user.email, password:user.password, returnSecureToken:true});
+    {email:user.email, password:user.password, returnSecureToken:true})
+    .pipe(
+      tap((userData:AuthResponseData)=>{
+        const expirationTime=new Date(new Date().getTime() + +userData.expiresIn*1000);
+        const user=new User(userData.localId, userData.email, userData.idToken, expirationTime);
+        this._user.next(user);
+      })
+      
+    );
   }
 
   logIn(user:UserData){
     this._isUserAuthenticated = true;
     return this.http.post<AuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`,
-    {email:user.email, password:user.password, returnSecureToken:true});
+    {email:user.email, password:user.password, returnSecureToken:true})
+    .pipe(
+      tap((userData:AuthResponseData)=>{
+        const expirationTime=new Date(new Date().getTime() + +userData.expiresIn*1000);
+        const user=new User(userData.localId, userData.email, userData.idToken, expirationTime);
+        this._user.next(user);
+      })
+      
+    );
   }
   
 
   logOut(){
     this._isUserAuthenticated = false;
   }
-  
+
 }
 
 
