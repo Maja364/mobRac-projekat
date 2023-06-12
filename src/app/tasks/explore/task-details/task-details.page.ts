70f02702dfd642f25a17554d947e79977c8befc8
Jovana Aleksic <jaleksic23@gmail.com>
2023-06-13 00:43:28 +0200

    zaobljene ivice polja

--- src/app/tasks/explore/task-details/task-details.page.ts
+++ src/app/tasks/explore/task-details/task-details.page.ts
@@ -10,7 +10,7 @@ import { TasksService } from '../../tasks.service';
 })
 export class TaskDetailsPage implements OnInit {
 
-  task: Task = {id: 't1', title: 'Projekat iz Mobilnog racunarstva', description: 'Projekat zavrsiti do 1. juna', imgUrl: 'https://i.pinimg.com/736x/ac/c2/9e/acc29eb7588b23d2cf548e32eff5ce9c.jpg',userId:'xx'};
+  task: Task = {id: 't1', title: 'Projekat iz Mobilnog racunarstva', description: 'Projekat zavrsiti do 1. juna', imgUrl: '',userId:'xx'};
 
   constructor(private route: ActivatedRoute, private tasksService: TasksService, private router:Router) { }
 
